bda1cf71115b05aed2d885b62cd3d44bc709b9f8
finn-nerd <walker.finn123@gmail.com>
2023-10-25 12:36:55 -0400

    added decode function

--- main.py
+++ main.py
@@ -2,11 +2,22 @@
 def print_menu():
     print("Menu \n-------------\n1. Encode\n2. Decode\n3. Quit")
 
+def decode_password(password):
+    output = ""
+    for digit in password:
+        decoded_digit = int(digit) - 3
+        if decoded_digit < 0:
+            output += str(decoded_digit + 10)
+        else:
+            output += str(decoded_digit)
+    print(password)
+    return output
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     password = ""
-    encoded_password = " "
+    encoded_password = ""
     while True:
         print_menu()
         userinput = input("Please enter an option")
@@ -18,6 +29,6 @@ if __name__ == '__main__':
             print("Your password has been encoded and stored!")
 
         if userinput == "2":
-            print(f"The encoded password is {encoded_password}, and the original password is {password}.")
+            print(f"The encoded password is {encoded_password}, and the original password is {decode_password(encoded_password)}.")
         if userinput == "3":
             break
